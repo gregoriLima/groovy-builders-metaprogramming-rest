69462cce4a14e7bbe247845b8451f91afb2549f5
Grégori Lima <gregorifl@gmail.com>
2022-01-28 05:06:36 -0300

    Regular Exp

--- src/features/RegularExpressions.groovy
+++ src/features/RegularExpressions.groovy
@@ -11,3 +11,29 @@ println pat.class // java.util.regex.Pattern
 // no need scape carachteres
 String slashyPattern = /a\b/
 println slashyPattern
+
+// ~ means same as line 5 and '/ ... /' is the same done at line 12
+def pattern = ~/a\b/
+println pattern.class // java.util.regex.Pattern
+
+
+// Find | Match
+def text = "Being a Cleveland Sports Fan is no way to go through life"
+pattern = ~/Cleveland Sports Fan/
+def finder = text =~ pattern
+def matcher = text ==~ pattern
+
+println finder // java.util.regex.Matcher[pattern=Cleveland Sports Fan region=0,57 lastmatch=]
+println finder.size() // 1
+
+println matcher // false
+
+text = "Cleveland Sports Fan"
+matcher = text ==~ pattern
+
+println matcher // true
+
+
+
+text = text.replaceFirst(pattern, "Buffalo Fan")
+println text // Buffalo Fan
